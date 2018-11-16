1dcb853773d63b35034ba27e8819ea2ddda710d5
Jimmy1007 <623492978@qq.com>
2018-11-16 08:21:59 +0800

    Initial Commit

---
+++ pages/index/index.js
@@ -0,0 +1,32 @@
+const weatherMap = {
+  'sunny': '晴天',
+  'cloudy': '多云',
+  'overcast': '阴',
+  'lightrain': '小雨',
+  'heavyrain': '大雨',
+  'snow': '雪'
+}
+Page({
+  onLoad(){
+    wx.request({
+      url: 'https://test-miniprogram.com/api/weather/now',
+      data: {
+        
+
+        city: '武汉市',
+      },
+      success:res=> {
+        console.log(res)
+        let result = res.data.result
+        let temp = result.now.temp
+        let weather = result.now.weather
+        console.log(temp,weather)
+        this.setData({
+          nowTemp:temp,
+          nowWeather:weatherMap[weather],
+          nowWeatherBackground:'/images/'+weather+'-bg.png'
+        })
+      }
+    })
+  }
+})
